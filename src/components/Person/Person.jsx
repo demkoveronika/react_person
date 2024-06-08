682a05658815c1aaee2d16fafd50105b28e59fd1
Veronika Demko <demkoveronika06@gmail.com>
2024-06-08 19:55:15 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,30 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import cn from 'classnames';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let partnerLabel;
+
+  if (isMarried) {
+    if (sex === 'm') {
+      partnerLabel = 'wife';
+    } else if (sex === 'f') {
+      partnerLabel = 'husband';
+    }
+  } else {
+    partnerLabel = 'not married';
+  }
+
+  const partnerText = cn({
+    [`${partnerName} is my ${partnerLabel}`]: isMarried && partnerName,
+    [`I am ${partnerLabel}`]: !isMarried || !partnerName,
+  });
+
+  return (
+    <div className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerText}</p>
+    </div>
+  );
+};
